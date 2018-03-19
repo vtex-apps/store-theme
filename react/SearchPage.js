de299eee7f347797f3dbe136ffde45123e894987
Leticia Wanderley <leticiafwanderley@gmail.com>
2018-03-19 17:48:20 -0300

    Fix components proptypes

--- react/SearchPage.js
+++ react/SearchPage.js
@@ -1,19 +1,17 @@
-import React, {Component} from 'react'
-import PropTypes from 'prop-types'
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 // eslint-disable-next-line
 export default class SearchPage extends Component {
   static propTypes = {
-    params: PropTypes.params,
-  }
+    params: PropTypes.object
+  };
 
   render() {
     return (
       <div className="pa4 ph7 pt0 georgia mw8 center f4 h-100">
-        <p>
-          {JSON.stringify(this.props)}
-        </p>
+        <p>{JSON.stringify(this.props)}</p>
       </div>
-    )
+    );
   }
 }
