b04b82850e1da4052c53f1bca63126d5811226a7
Leticia Wanderley <leticiafwanderley@gmail.com>
2018-03-27 21:22:43 -0300

    Extract products query

--- react/Shelf.js
+++ react/Shelf.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { graphql } from 'react-apollo'
-import gql from 'graphql-tag'
+import productsQuery from './productsQuery.gql'
 
 import ShelfItem from './ShelfItem'
 
 function Shelf({ data }) {
   return (
-    <div className="flex flex-row">
+    <div className="flex flex-row overflow-x-auto">
       {!data.loading &&
         data.products.map(product => (
           <ShelfItem
@@ -25,140 +25,6 @@ function Shelf({ data }) {
 Shelf.propTypes = {
   data: PropTypes.object,
 }
-
-const query = gql`
-  query Products(
-    $query: String
-    $category: String
-    $specificationFilters: [String]
-    $priceRange: String
-    $collection: String
-    $orderBy: String
-    $from: Int
-    $to: Int
-    $salesChannel: String
-  ) {
-    products(
-      query: $query
-      category: $category
-      specificationFilters: $specificationFilters
-      priceRange: $priceRange
-      collection: $collection
-      orderBy: $orderBy
-      from: $from
-      to: $to
-      salesChannel: $salesChannel
-    ) {
-      productId
-      productName
-      brand
-      linkText
-      productReference
-      categoryId
-      categories
-      categoriesIds
-      clusterHighlights {
-        id
-        name
-      }
-      link
-      description
-      items {
-        itemId
-        name
-        nameComplete
-        complementName
-        ean
-        referenceId {
-          Key
-          Value
-        }
-        measurementUnit
-        unitMultiplier
-        images {
-          imageId
-          imageLabel
-          imageTag
-          imageUrl
-          imageText
-        }
-        sellers {
-          sellerId
-          sellerName
-          addToCartLink
-          sellerDefault
-          commertialOffer {
-            Installments {
-              Value
-              InterestRate
-              TotalValuePlusInterestRate
-              NumberOfInstallments
-              PaymentSystemName
-              PaymentSystemGroupName
-              Name
-            }
-            Price
-            ListPrice
-            PriceWithoutDiscount
-            RewardValue
-            PriceValidUntil
-            AvailableQuantity
-            Tax
-            CacheVersionUsedToCallCheckout
-            DeliverySlaSamples {
-              DeliverySlaPerTypes {
-                TypeName
-                Price
-                EstimatedTimeSpanToDelivery
-              }
-              Region {
-                IsPersisted
-                IsRemoved
-                Id
-                Name
-                CountryCode
-                ZipCode
-                CultureInfoName
-              }
-            }
-          }
-        }
-        variations {
-          name
-          values
-        }
-        attachments {
-          id
-          name
-          required
-          domainValues {
-            FieldName
-            MaxCaracters
-            DomainValues
-          }
-        }
-      }
-      properties {
-        name
-        values
-      }
-      propertyGroups {
-        name
-        properties
-      }
-      recommendations {
-        buy {
-          productId
-          productName
-        }
-        view {
-          productId
-          productName
-        }
-      }
-    }
-  }
-`
 
 const options = {
   options: ({
@@ -186,7 +52,7 @@ const options = {
   }),
 }
 
-const component = graphql(query, options)(Shelf)
+const component = graphql(productsQuery, options)(Shelf)
 
 component.schema = {
   title: 'Shelf',
