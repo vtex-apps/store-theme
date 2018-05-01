ea7eba7b327da55850981144a855418909bc8148
Gabriel Galc <gabriel.galc@vtex.com.br>
2018-05-01 17:02:28 -0300

    Improve footer

--- react/components/Footer.js
+++ react/components/Footer.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 import { injectIntl, intlShape } from 'react-intl'
 
+import VTEXIcon from '../images/VTEXIcon'
+import CardIcon from '../images/CardIcon'
+
 class Footer extends Component {
   static propTypes = {
     intl: intlShape.isRequired,
@@ -11,12 +14,23 @@ class Footer extends Component {
   render() {
     const year = new Date().getFullYear()
     return (
-      <footer className="w-100 left-0 pa7 pa9-l bg-near-white flex items-center">
-        <div className="mw8 center">
-          <div className="cf tc fw7 flex items-center justify-center">
+      <footer className="w-100 left-0 bg-serious-black">
+        <div className="center white flex items-center justify-between w-100 items-center f6 ph9 pt9">
+          <div>All rights reserved | {year}</div>
+          <div className="cf tc fw7 flex items-center justify-center f5">
             {this.translate('poweredBy')}
+            <VTEXIcon />
           </div>
-          <div className="tc">{year}</div>
+        </div>
+        <div className="ph9 pb9 flex justify-between items-end">
+          <div className="white">
+            <a className="db pv2 fw5">About us</a>
+            <a className="db pv2 fw5">Privacy policy</a>
+            <a className="db pv2 fw5">Terms and conditions</a>
+          </div>
+          <div className="mr4">
+            <CardIcon />
+          </div>
         </div>
       </footer>
     )
