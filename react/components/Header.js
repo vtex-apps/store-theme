db8102596bb1e69bbe6532480a14720e0ae3680b
Felipe Sales <sales.felipeb@gmail.com>
2018-05-10 18:37:37 -0300

    Add the search bar component

--- react/components/Header.js
+++ react/components/Header.js
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types'
 import { injectIntl, intlShape } from 'react-intl'
 import Input from '@vtex/styleguide/lib/Input'
 import Button from '@vtex/styleguide/lib/Button'
+import SearchBar from 'vtex.storecomponents/SearchBar'
+
 import { ExtensionPoint } from 'render'
 
 import SearchIcon from '../images/SearchIcon'
@@ -39,35 +41,20 @@ class Header extends Component {
         </div>
         <div className="z-2 flex-ns items-center w-100 top-0 pa4 pa5-ns ph7-l bg-white tl">
           <div className="flex items-center justify-between pb3 pb0-ns">
-            <a className="link b f3 near-black tc tl-ns pr7 serious-black" href="/">
+            <a
+              className="link b f3 near-black tc tl-ns pr7 serious-black"
+              href="/"
+            >
               {name || account}
             </a>
           </div>
           <div className="flex items-center flex-auto">
             <div className="w-100 flex ph7-ns">
               <div className="w-100">
-                <Input
+                <SearchBar
                   placeholder={this.translate('search-placeholder')}
-                  value={searchValue}
-                  onChange={this.handleChange}
-                  size="large"
+                  emptyPlaceholder={this.translate('search-emptyPlaceholder')}
                 />
-              </div>
-              <div>
-                <Button
-                  data-test-id="search"
-                  onClick={this.handleSearch}
-                  size="large"
-                >
-                  <div className="flex items-start">
-                    <div>
-                      <SearchIcon />
-                    </div>
-                    <div className="pl3 blue">
-                      SEARCH
-                    </div>
-                  </div>
-                </Button>
               </div>
             </div>
             <div>
