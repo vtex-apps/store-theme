22b536910d28ebe32b6f19d304b14de2a4962b0d
Andre Abrantes <andredossantosabrantes@gmail.com>
2018-05-21 15:35:53 -0300

    Fix topbar when scrolled overlapped the VTEX-Topbar

--- react/components/TopMenu.js
+++ react/components/TopMenu.js
@@ -9,7 +9,7 @@ const TopMenu = ({ name, intl, fixed }) => {
   const translate = id => intl.formatMessage({ id: `dreamstore.${id}` })
 
   return (
-    <div className={`${fixed ? 'fixed shadow-5' : ''} z-max flex flex-wrap w-100 top-0 pa4 pa5-ns ph7-l bg-white tl`}>
+    <div className={`${fixed ? 'fixed mt4 top-2 left-0 right-0 shadow-5' : 'top-0'} z-999 flex flex-wrap w-100 pa4 pa5-ns ph7-l bg-white tl`}>
       <div className="flex pa4">
         <a className="link b f3 near-black tc tl-ns serious-black" href="/">
           {name}
