dd57270c55d817d37d35fa15c4546ae976d26027
Felipe Sales <sales.felipeb@gmail.com>
2018-06-05 18:43:20 -0300

    Integrate the login component

--- react/components/TopMenu.js
+++ react/components/TopMenu.js
@@ -11,7 +11,7 @@ const TopMenu = ({ logoUrl, logoTitle, intl, fixed }) => {
     <div
       className={`${
         fixed ? 'fixed mt4 top-2 shadow-5' : 'top-0'
-        } z-999 flex flex-wrap w-100 pa4 pa5-ns ph7-l bg-white tl`}
+        } z-999 flex items-center flex-wrap w-100 pa4 pa5-ns ph8-l bg-white tl`}
     >
       <div className="flex pa4">
         <a className="link b f3 near-black tc tl-ns serious-black flex" href="/">
@@ -31,7 +31,7 @@ const TopMenu = ({ logoUrl, logoTitle, intl, fixed }) => {
           </div>
         </div>
       </div>
-      <div className="flex items-center flex-auto absolute top-3 right-1 pv4">
+      <div className="flex items-center flex-auto absolute top-3 right-1">
         <ExtensionPoint id="login" />
         <ExtensionPoint id="minicart" />
       </div>
