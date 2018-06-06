e5b4f718df8fcdbd370795fb2c26a7a00ccf43a1
Claudivan Filho <claudivan.filho@splab.ufcg.edu.br>
2018-06-06 11:07:35 -0300

    Add integration with vtex.search-result

--- react/__tests__/productsQuery.test.js
+++
@@ -1,7 +0,0 @@
-import productsQuery from '../queries/productsQuery.gql'
-
-describe('productsQuery test', () => {
-  it('should be the correct query', () => {
-    expect(productsQuery).toMatchSnapshot()
-  })
-})
