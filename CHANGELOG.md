799541726143b127f9bc11291385d37ace20a051
Felipe Sales <sales.felipeb@gmail.com>
2018-06-08 22:54:29 -0300

    Integrate the login component (#41)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,9 +6,9 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
-
 ### Added
-- Integration of logo component.
+- _Login_ component to the _Header_.
+- Add `vtex.styleguide` dependency.
 
 ## [1.0.0] - 2018-6-4
 ### Added
--- react/components/TopMenu.js
+++ react/components/TopMenu.js
@@ -1,26 +1,28 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { intlShape, injectIntl } from 'react-intl'
-import SearchBar from 'vtex.storecomponents/SearchBar'
-import Logo from 'vtex.store-components/Logo'
-
+import { Logo, SearchBar } from 'vtex.store-components'
 import { ExtensionPoint } from 'render'
 
 const TopMenu = ({ logoUrl, logoTitle, intl, fixed }) => {
   const translate = id => intl.formatMessage({ id: `dreamstore.${id}` })
 
   return (
-    <div className={`${fixed ? 'fixed mt4 top-2 shadow-5' : 'top-0'} z-999 flex flex-wrap justify-center w-100 pa4 pa5-ns ph7-l bg-white tl`}>
+    <div
+      className={`${
+        fixed ? 'fixed mt4 top-2 shadow-5' : 'top-0'
+        } z-999 flex items-center flex-wrap w-100 pa4 pa5-ns ph8-l bg-white tl`}
+    >
       <div className="flex pa4">
         <a className="link b f3 near-black tc tl-ns serious-black flex" href="/">
-          <Logo 
+          <Logo
             url={logoUrl}
             title={logoTitle}
           />
         </a>
       </div>
-      <div className="vtex-searchbar__container flex items-center flex-auto">
-        <div className="w-100 flex pr8-ns">
+      <div className="flex items-center flex-auto">
+        <div className="w-100 flex pr8-ns ma5">
           <div className="w-100">
             <SearchBar
               placeholder={translate('search-placeholder')}
@@ -29,7 +31,8 @@ const TopMenu = ({ logoUrl, logoTitle, intl, fixed }) => {
           </div>
         </div>
       </div>
-      <div className="absolute top-3 right-1 pv4">
+      <div className="flex items-center flex-auto absolute top-3 right-1">
+        <ExtensionPoint id="login" />
         <ExtensionPoint id="minicart" />
       </div>
     </div>
