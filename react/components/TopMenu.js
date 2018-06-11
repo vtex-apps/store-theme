a863d2b975ffc077f45c6a3d05681ba105aca940
Bruno Dias <bruno.dias@vtex.com.br>
2018-06-11 13:34:16 -0300

    Fix Top Menu CSS

--- react/components/TopMenu.js
+++ react/components/TopMenu.js
@@ -21,17 +21,13 @@ const TopMenu = ({ logoUrl, logoTitle, intl, fixed }) => {
           />
         </a>
       </div>
-      <div className="flex items-center flex-auto">
-        <div className="w-100 flex pr8-ns ma5">
-          <div className="w-100">
-            <SearchBar
-              placeholder={translate('search-placeholder')}
-              emptyPlaceholder={translate('search-emptyPlaceholder')}
-            />
-          </div>
-        </div>
+      <div className="flex-auto ma3">
+        <SearchBar
+          placeholder={translate('search-placeholder')}
+          emptyPlaceholder={translate('search-emptyPlaceholder')}
+        />
       </div>
-      <div className="flex items-center flex-auto absolute top-3 right-1">
+      <div className="flex items-center top-3 right-1">
         <ExtensionPoint id="login" />
         <ExtensionPoint id="minicart" />
       </div>
