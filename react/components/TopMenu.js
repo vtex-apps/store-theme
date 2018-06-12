26b72b821682d88bca39d81573930347a027c042
Andre Abrantes <andredossantosabrantes@gmail.com>
2018-06-12 11:47:35 -0300

    Responsive Top Menu layout

--- react/components/TopMenu.js
+++ react/components/TopMenu.js
@@ -13,23 +13,23 @@ const TopMenu = ({ logoUrl, logoTitle, intl, fixed }) => {
         fixed ? 'fixed mt4 top-2 shadow-5' : 'top-0'
         } z-999 flex items-center w-100 flex-wrap pa4 pa5-ns bg-white tl`}
     >
-      <div className="flex pa4">
-        <a className="link b f3 near-black tc tl-ns serious-black flex" href="/">
+      <div className="flex w-100 w-auto-ns pa4-ns items-center">
+        <a className="link b f3 near-black tc tl-ns serious-black flex-auto" href="/">
           <Logo
             url={logoUrl}
             title={logoTitle}
           />
         </a>
       </div>
-      <div className="flex-auto ma3">
+      <div className="flex-auto pr2 pa4">
         <SearchBar
           placeholder={translate('search-placeholder')}
           emptyPlaceholder={translate('search-emptyPlaceholder')}
         />
       </div>
-      <div className="flex items-center top-3 right-1">
+      <div className="pr2 bg-black">
+        <ExtensionPoint id="minicart"/>
         <ExtensionPoint id="login" />
-        <ExtensionPoint id="minicart" />
       </div>
     </div>
   )
