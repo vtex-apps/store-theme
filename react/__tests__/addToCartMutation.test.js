4288f903d399905d3519a95928b7eb0d0b7450e0
Bruno Dias <bruno.dias@vtex.com.br>
2018-09-26 13:37:38 -0300

    Remove unused queries and tests.

--- react/__tests__/addToCartMutation.test.js
+++
@@ -1,7 +0,0 @@
-import addToCartMutation from '../mutations/addToCartMutation.gql'
-
-describe('addToCartMutation test', () => {
-  it('should be the correct mutation', () => {
-    expect(addToCartMutation).toMatchSnapshot()
-  })
-})
