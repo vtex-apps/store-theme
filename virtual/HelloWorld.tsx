ea5ef662fe1db12da88828ce32b496d3e5ca9d1d
Breno Calazans <brenocalazans@gmail.com>
2020-08-06 14:59:26 -0300

    WIP

---
+++ virtual/HelloWorld.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const HelloWorld = () => {
+  return (
+    <>
+      <h1>Hello World</h1>
+      <button onClick={() => {}}>a</button>
+      <h1>Hello World</h1>
+      <h1>Hello World</h1>
+      <h1>Hello World</h1>
+      <h1>Hello World</h1>
+      <h1>Hello World</h1>
+    </>
+  )
+}
+
+export default HelloWorld
