bf72b9b0be19b17c62fb5745f33c926b97d3df78
Breno Calazans <brenocalazans@gmail.com>
2020-08-14 18:27:47 -0300

    WIP

--- virtual/ProductSpecifications.tsx
+++ virtual/ProductSpecifications.tsx
@@ -1,40 +1,44 @@
-import * as React from "react";
+import * as React from 'react'
 import {
   ProductSpecificationGroup,
   ProductSpecification,
   ProductSpecificationValues,
-  ProductSpecificationText
-} from "vtex.product-specifications";
+  ProductSpecificationText,
+} from 'vtex.product-specifications'
+
 import styles from './ProductSpecifications.css'
 
-interface Props {
-  text?: string
-}
+const ProductSpecs = () => {
+  const _state = React.useState(1)
+  console.log(_state)
 
-const ProductSpecs = (props: Props) => {
   return (
     <ProductSpecificationGroup>
       <div className={styles.productSpecs}>
-        <div className={`bg-muted-4 pa3 t-heading-4 mb2 `}>
-          <ProductSpecificationText message={`{groupName}`} />
+        <div className="bg-muted-4 pv3 ph5 t-heading-5 mb2">
+          <ProductSpecificationText message="{groupName}" />
         </div>
         <ProductSpecification>
-          <div className="flex pa3">
-            <div className="w-50 w-25-ns pr1">
-            <ProductSpecificationText message={`{specificationName}`} />
+          <div className={`flex pv5 ph5 ${styles.productSpecLine}`}>
+            <div className="w-50 w-25-ns pr1 b">
+              <ProductSpecificationText message="{specificationName}" />
             </div>
             <div className="w-50 w-75-ns">
-            <ProductSpecificationValues>
-              <ProductSpecificationText message={`{specificationValue}`} />
-            </ProductSpecificationValues>
+              <ProductSpecificationValues>
+                <ProductSpecificationText message="{specificationValue}" />
+              </ProductSpecificationValues>
             </div>
           </div>
         </ProductSpecification>
       </div>
     </ProductSpecificationGroup>
-  );
-};
+  )
+}
 
-ProductSpecs.displayName = "product-specs";
+ProductSpecs.displayName = 'product-specs'
 
-export default ProductSpecs;
+ProductSpecs.schema = {
+
+}
+
+export default ProductSpecs
