3a538a3d65e18573f6ef2143f1206011d4a0990b
Breno Calazans <brenocalazans@gmail.com>
2020-12-17 16:09:22 -0300

    WIP

---
+++ react/MyCustomBlock.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { FormattedCurrency } from 'vtex.format-currency'
+import { useCssHandles, useCustomClasses } from 'vtex.css-handles'
+
+import style from './style.css'
+import styleModule from './style.module.css'
+import './style.global.css'
+import './global.css'
+import './theme.css'
+
+function MyCustomBlock() {
+  const formattedClasses = useCustomClasses(() => ({
+    currencyContainer: [style.currency, styleModule.currency],
+    currencyCode: ['globalCurrencyCode', 'styleGlobalCurrencyCode', 'themeGlobalCurrencyCode']
+  }))
+
+  return (
+    <div>
+      Hello
+      <FormattedCurrency value={10} classes={formattedClasses} />
+    </div>
+  )
+}
+
+export default MyCustomBlock
